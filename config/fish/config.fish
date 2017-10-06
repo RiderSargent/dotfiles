34dd65d3dc273be370d69e42ef4135bac714b467
Rider Sargent <rider.sargent@gmail.com>
2017-10-06 14:30:24 +0100

    Fish - set EDITOR correctly and added newline to prompt.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,14 +4,14 @@ source /usr/local/share/chruby/auto.fish
 set PATH $PATH ~/bin
 set PATH $PATH ~/.dotfiles/bin
 
-set EDITOR nvim
+setenv EDITOR nvim
 
 # set -g fish_prompt_pwd_dir_length 3
 
 # bobthefish settings
 # set theme_color_scheme terminal-dark
 set -g theme_display_ruby yes
-# set -g theme_newline_cursor yes
+set -g theme_newline_cursor yes
 
 
 # --- Aliases ------------------------------------------------------------------
