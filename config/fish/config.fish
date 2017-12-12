37b83804799f393cb3349ac771579f95bb8a2bfe
Rider Sargent <rider.sargent@gmail.com>
2017-12-12 10:21:26 +0000

    Changed colorscheme to Solarized Dark (iTerm, Tmux and NeoVim).

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -54,38 +54,39 @@ end
 # set fish_pager_color_progress 'brwhite'  '--background=cyan'
 
 
-# brcyan is the very light grey in Solarized color scheme
-set fish_color_autosuggestion brcyan
-set fish_color_command --bold
-set fish_color_comment red
-set fish_color_cwd green
-set fish_color_cwd_root red
-set fish_color_end brmagenta
-set fish_color_error brred
-set fish_color_escape 'bryellow'  '--bold'
-set fish_color_history_current --bold
-set fish_color_host normal
-set fish_color_match --background=brblue
-set fish_color_normal normal
-set fish_color_operator blue
-set fish_color_param cyan
-set fish_color_quote yellow
-set fish_color_redirection brblue
-set fish_color_search_match 'bryellow'  '--background=white'
-set fish_color_selection 'white'  '--bold'  '--background=brblack'
-set fish_color_status red
-set fish_color_user brgreen
-set fish_color_valid_path --underline
+# # brcyan is the very light grey in Solarized color scheme
+# set fish_color_autosuggestion brcyan
+# set fish_color_command --bold
+# set fish_color_comment red
+# set fish_color_cwd green
+# set fish_color_cwd_root red
+# set fish_color_end brmagenta
+# set fish_color_error brred
+# set fish_color_escape 'bryellow'  '--bold'
+# set fish_color_history_current --bold
+# set fish_color_host normal
+# set fish_color_match --background=brblue
+# set fish_color_normal normal
+# set fish_color_operator blue
+# set fish_color_param cyan
+# set fish_color_quote yellow
+# set fish_color_redirection brblue
+# set fish_color_search_match 'bryellow'  '--background=white'
+# set fish_color_selection 'white'  '--bold'  '--background=brblack'
+# set fish_color_status red
+# set fish_color_user brgreen
+# set fish_color_valid_path --underline
 
-set fish_pager_color_completion cyan
-set fish_pager_color_description 'brcyan'
-set fish_pager_color_prefix brgreen
-set fish_pager_color_progress 'brwhite'  '--background=cyan'
+# set fish_pager_color_completion cyan
+# set fish_pager_color_description 'brcyan'
+# set fish_pager_color_prefix brgreen
+# set fish_pager_color_progress 'brwhite'  '--background=cyan'
 
 
 
 # bobthefish settings
-set theme_color_scheme terminal-light
+# set theme_color_scheme terminal-light
+set theme_color_scheme solarized-dark
 # set theme_color_scheme solarized-light
 set -g theme_display_ruby yes
 set -g theme_newline_cursor yes
