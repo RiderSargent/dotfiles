5cd040a8e22f9c254435bf593aafc76d87220138
Rider Sargent <rider.sargent@gmail.com>
2018-09-28 12:41:53 +0100

    Hammerspoon - added jhkl, maximized and 'regular' window resize/move hotkeys.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,24 +1,114 @@
--- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
---   hs.alert.show("Hello World!")
--- end)
+-- Show screen and window info
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "0", function()
+  local focusedWindow = hs.window.focusedWindow()
+  local focusedWindowFrame = focusedWindow:frame()
+  local screen = focusedWindow:screen()
+  local screenFrame = screen:frame()
 
-hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "j", function()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+  text = "screenFrame.x: " .. screenFrame.x .. "\nscreenFrame.y: " .. screenFrame.y .. "\nfocusedWindowFrame.x: " .. focusedWindowFrame.x .. "\nfocusedWindowFrame.y: " .. focusedWindowFrame.y .. "\nfocusedWindowFrame.h: " .. focusedWindowFrame.h .. "\nfocusedWindowFrame.w: " .. focusedWindowFrame.w .. "."
+
+  hs.alert.show(text)
 end)
 
+-- Window to full screen
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "1", function()
+  local focusedWindow = hs.window.focusedWindow()
+  local focusedWindowFrame = focusedWindow:frame()
+  local screen = focusedWindow:screen()
+  local screenFrame = screen:frame()
+
+  focusedWindowFrame.x = screenFrame.x
+  focusedWindowFrame.y = screenFrame.y
+  focusedWindowFrame.w = screenFrame.w
+  focusedWindowFrame.h = screenFrame.h
+  focusedWindow:setFrame(focusedWindowFrame)
+end)
+
+-- Window to centered 1440 by 900
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "2", function()
+  local focusedWindow = hs.window.focusedWindow()
+  local focusedWindowFrame = focusedWindow:frame()
+  local screen = focusedWindow:screen()
+  local screenFrame = screen:frame()
+
+  local width = 1440
+  local height = 900
+
+  focusedWindowFrame.x = screenFrame.x + (screenFrame.w / 2 - 720)
+  focusedWindowFrame.y = screenFrame.y + (screenFrame.h / 2 - 488)
+  focusedWindowFrame.w = width
+  focusedWindowFrame.h = height
+  focusedWindow:setFrame(focusedWindowFrame)
+end)
+
+-- Window to left half of screen
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "h", function()
+  local focusedWindow = hs.window.focusedWindow()
+  local focusedWindowFrame = focusedWindow:frame()
+  local screen = focusedWindow:screen()
+  local screenFrame = screen:frame()
+
+  focusedWindowFrame.x = screenFrame.x
+  focusedWindowFrame.y = screenFrame.y
+  focusedWindowFrame.w = screenFrame.w / 2
+  focusedWindowFrame.h = screenFrame.h
+  focusedWindow:setFrame(focusedWindowFrame)
+end)
+
+-- Window to top half of screen
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "k", function()
-  -- hs.alert.show(hs.application.frontmostApplication():title() .. " Center", show_window_notification_duration)
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+  local focusedWindow = hs.window.focusedWindow()
+  local focusedWindowFrame = focusedWindow:frame()
+  local screen = focusedWindow:screen()
+  local screenFrame = screen:frame()
 
-  f.w = 1440
-  f.h = 900
-  f.x = 10
-  f.y = 100
-
-  hs.notify.new({title="Hammerspoon", informativeText="max.x:" .. max.x .. ", max.y:" .. max.y .. ", f.x:" .. f.x .. ", f.y:" .. f.y .. "."}):send()
-
-  win:setFrame(f)
+  focusedWindowFrame.x = screenFrame.x
+  focusedWindowFrame.y = screenFrame.y
+  focusedWindowFrame.w = screenFrame.w
+  focusedWindowFrame.h = screenFrame.h / 2
+  focusedWindow:setFrame(focusedWindowFrame)
 end)
+
+-- Window to bottom half of screen
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "j", function()
+  local focusedWindow = hs.window.focusedWindow()
+  local focusedWindowFrame = focusedWindow:frame()
+  local screen = focusedWindow:screen()
+  local screenFrame = screen:frame()
+
+  focusedWindowFrame.x = screenFrame.x
+  focusedWindowFrame.y = screenFrame.y + (screenFrame.h / 2)
+  focusedWindowFrame.w = screenFrame.w
+  focusedWindowFrame.h = screenFrame.h / 2
+  focusedWindow:setFrame(focusedWindowFrame)
+end)
+
+-- Window to right half of screen
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "l", function()
+  local focusedWindow = hs.window.focusedWindow()
+  local focusedWindowFrame = focusedWindow:frame()
+  local screen = focusedWindow:screen()
+  local screenFrame = screen:frame()
+
+  focusedWindowFrame.x = screenFrame.x + (screenFrame.w / 2)
+  focusedWindowFrame.y = screenFrame.y
+  focusedWindowFrame.w = screenFrame.w / 2
+  focusedWindowFrame.h = screenFrame.h
+  focusedWindow:setFrame(focusedWindowFrame)
+end)
+
+-- Reload Config Automatically
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
