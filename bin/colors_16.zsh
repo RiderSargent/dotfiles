995408153ef727eb6d0c8727bd4e9a9595583fc3
Rider Sargent <rider.sargent@gmail.com>
2019-03-05 11:22:09 +0000

    Zsh - Added colors_16.zsh script.

---
+++ bin/colors_16.zsh
@@ -0,0 +1,11 @@
+#! /usr/local/bin/zsh
+
+print    "Code         Normal Bright (code + 8)"
+print -P "000: %F{000}  black ██████%f %F{008}██████ black bold%f"
+print -P "001: %F{001}    red ██████%f %F{009}██████ red bold%f"
+print -P "002: %F{002}  green ██████%f %F{010}██████ green bold%f"
+print -P "003: %F{003} yellow ██████%f %F{011}██████ yellow bold%f"
+print -P "004: %F{004}   blue ██████%f %F{012}██████ blue bold%f"
+print -P "005: %F{005}magenta ██████%f %F{013}██████ magenta bold%f"
+print -P "006: %F{006}   cyan ██████%f %F{014}██████ cyan bold%f"
+print -P "007: %F{007}  white ██████%f %F{015}██████ white bold%f"
